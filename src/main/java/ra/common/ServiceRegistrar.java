ab028e8640f2764ebfb794607930e39616d68b51
objectorange <brian@resolvingarchitecture.io>
2020-06-15 10:52:03 -0700

    return success status

--- src/main/java/ra/common/ServiceRegistrar.java
+++ src/main/java/ra/common/ServiceRegistrar.java
@@ -5,13 +5,11 @@ import java.util.Properties;
 
 /**
  * The responsibility to register and unregister a service along with its observers.
- *
- * @author objectorange
  */
 public interface ServiceRegistrar {
 
-    void registerService(Class serviceClass, Properties properties, List<ServiceStatusObserver> observers)
+    boolean registerService(Class serviceClass, Properties properties, List<ServiceStatusObserver> observers)
             throws ServiceNotAccessibleException, ServiceNotSupportedException, ServiceRegisteredException;
 
-    void unregisterService(Class serviceClass);
+    boolean unregisterService(Class serviceClass);
 }
