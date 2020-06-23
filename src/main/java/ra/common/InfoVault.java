7f6bdfe0a6ce7a1f2528832bc2332dd7d7cb0036
objectorange <brian@resolvingarchitecture.io>
2020-06-22 22:14:27 -0700

    add

---
+++ src/main/java/ra/common/InfoVault.java
@@ -0,0 +1,9 @@
+package ra.common;
+
+import ra.common.content.Content;
+
+public class InfoVault {
+    public String location;
+    public Content content;
+    public Boolean storeExternal = false;
+}
