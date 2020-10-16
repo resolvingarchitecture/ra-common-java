4cd996d8f36aeae5aa7f991145b00233a070599a
objectorange <brian@resolvingarchitecture.io>
2020-10-16 00:06:24 -0400

    cleanup

--- src/main/java/ra/common/service/Notification.java
+++ src/main/java/ra/common/service/Notification.java
@@ -1,8 +1,5 @@
 package ra.common.service;
 
-import ra.common.service.Operation;
-import ra.common.service.Packet;
-
 public interface Notification extends Operation {
     void notify(Packet packet);
 }
