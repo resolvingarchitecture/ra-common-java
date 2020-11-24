bf76a23167325420a213d9d92bd6ebd262e16b02
objectorange <brian@resolvingarchitecture.io>
2020-11-23 23:14:56 -0500

    added commands to use during control requests

---
+++ src/main/java/ra/common/ControlCommand.java
@@ -0,0 +1,20 @@
+package ra.common;
+
+public enum ControlCommand {
+    InitiateComm,
+    Start,
+    RegisterService,
+    UnregisterService,
+    StartService,
+    PauseService,
+    UnPauseService,
+    RestartService,
+    StopService,
+    GracefullyStopService,
+    Pause,
+    UnPause,
+    Restart,
+    Shutdown,
+    GracefullyShutdown,
+    CloseClient
+}
