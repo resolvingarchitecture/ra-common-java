051958e5da531ae3634b0abe30fd0cccab0d6067
objectorange <brian@resolvingarchitecture.io>
2020-12-30 20:29:17 -0500

    add fees

--- src/main/java/ra/common/currency/crypto/BTC.java
+++ src/main/java/ra/common/currency/crypto/BTC.java
@@ -5,6 +5,34 @@ import ra.common.currency.StoreOfValue;
 
 public class BTC extends BaseCoin implements StoreOfValue {
 
+    private long fastestFee;
+    private long halfHourFee;
+    private long hourFee;
+
+    public long getFastestFee() {
+        return fastestFee;
+    }
+
+    public void setFastestFee(long fastestFee) {
+        this.fastestFee = fastestFee;
+    }
+
+    public long getHalfHourFee() {
+        return halfHourFee;
+    }
+
+    public void setHalfHourFee(long halfHourFee) {
+        this.halfHourFee = halfHourFee;
+    }
+
+    public long getHourFee() {
+        return hourFee;
+    }
+
+    public void setHourFee(long hourFee) {
+        this.hourFee = hourFee;
+    }
+
     @Override
     public Long maxSupply() {
         return 2100000000000000L;
