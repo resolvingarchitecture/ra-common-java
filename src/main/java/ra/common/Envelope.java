e644761904d97b93fe7575b64b7f9549fc60544d
objectorange <brian@resolvingarchitecture.io>
2021-01-02 00:07:13 -0500

    additional helper method

--- src/main/java/ra/common/Envelope.java
+++ src/main/java/ra/common/Envelope.java
@@ -339,6 +339,11 @@ public final class Envelope extends JSON {
         return true;
     }
 
+    public boolean addExternalRoute(String service, String operation) {
+        dynamicRoutingSlip.addRoute(new SimpleExternalRoute(service, operation));
+        return true;
+    }
+
     public boolean addExternalRoute(Class service, String operation, NetworkPeer origination, NetworkPeer destination) {
         dynamicRoutingSlip.addRoute(new SimpleExternalRoute(service.getName(), operation, origination, destination));
         return true;
