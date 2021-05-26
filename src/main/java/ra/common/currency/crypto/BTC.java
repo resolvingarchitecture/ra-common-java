a2dcd99cd1e488498d90f70e652a71d59dc461f5
objectorange <brian@resolvingarchitecture.io>
2021-05-26 00:00:24 -0400

    added sats support

--- src/main/java/ra/common/currency/crypto/BTC.java
+++ src/main/java/ra/common/currency/crypto/BTC.java
@@ -2,10 +2,38 @@ package ra.common.currency.crypto;
 
 import ra.common.currency.StoreOfValue;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
 public class BTC extends Crypto implements StoreOfValue {
 
+    public final static Double MAX_SUPPLY = 21000000.00000000D;
+    public final static Integer PRECISION = 8;
+    public final static Integer SATS_PER_BITCOIN = 100000000;
+
     public BTC() {
-        super(21000000.00000000, 8);
+        super(MAX_SUPPLY, PRECISION);
+    }
+
+    public BTC(Double valueInBTC) {
+        super(MAX_SUPPLY, PRECISION);
+        String tempValue = valueInBTC.toString();
+        int numberTrailingZeroes = (tempValue.length()-1) - tempValue.indexOf(".");
+        int numberToAdd = PRECISION - numberTrailingZeroes;
+        for(int i=0; i<numberToAdd; i++) {
+            tempValue+="0";
+        }
+        tempValue = tempValue.replace(".","");
+        long sats = Long.parseLong(tempValue);
+        super.setValue(BigInteger.valueOf(sats));
+    }
+
+    public BigDecimal valueInBitcoin() {
+        if(value()==null || value().equals(BigInteger.ZERO)) return BigDecimal.ZERO;
+        BigDecimal bitcoin = new BigDecimal(value().longValue());
+        BigDecimal sats = new BigDecimal(SATS_PER_BITCOIN);
+        return bitcoin.divide(sats);
     }
 
     @Override
