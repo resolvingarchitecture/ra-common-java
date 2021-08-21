b1f12c82ab5d3a3a536b5f461c88913a201a1d67
objectorange <brian@resolvingarchitecture.io>
2021-08-21 13:40:36 -0400

    added JSONSerializable support

--- src/main/java/ra/common/JSONParser.java
+++ src/main/java/ra/common/JSONParser.java
@@ -12,22 +12,19 @@ public class JSONParser {
         return parse(new String(Files.readAllBytes(path)));
     }
 
-    public static Object parse(Object json)
-    {
+    public static Object parse(Object json) {
         if (json == null)
             return null;
         return parse(json.toString());
     }
 
-    public static Object parse(String json)
-    {
+    public static Object parse(String json) {
         if (json == null)
             return null;
         return parse(json, new int[1]);
     }
 
-    public static List<Object> parseStream(String json)
-    {
+    public static List<Object> parseStream(String json) {
         if (json == null)
             return null;
         int[] pos = new int[1];
@@ -38,34 +35,27 @@ public class JSONParser {
         return res;
     }
 
-    public static String toString(Object obj)
-    {
+    public static String toString(Object obj) {
         StringBuffer buf = new StringBuffer();
         toString(obj, buf);
         return buf.toString();
     }
 
-    public static String stripWhitespace(String src)
-    {
+    public static String stripWhitespace(String src) {
         boolean inQuote = false, isEscaped = false;
         StringBuffer buf = new StringBuffer();
 
-        for (int i=0; i<src.length(); i++)
-        {
+        for (int i=0; i<src.length(); i++) {
             char ch = src.charAt(i);
 
-            if (!inQuote)
-            {
-                if (ch == '"')
-                {
+            if (!inQuote) {
+                if (ch == '"') {
                     inQuote = true;
                     isEscaped = false;
                 }
                 else if (Character.isWhitespace(ch))
                     continue;
-            }
-            else if (inQuote)
-            {
+            } else if (inQuote) {
                 if (ch == '\\')
                     isEscaped = !isEscaped;
                 else if ((ch == '"') && !isEscaped)
@@ -78,25 +68,19 @@ public class JSONParser {
         return buf.toString();
     }
 
-    public static Object getValue(Object json, String path)
-    {
+    public static Object getValue(Object json, String path) {
         String[] parts = path.split("\\.");
 
-        for (int i=0; i<parts.length; i++)
-        {
+        for (int i=0; i<parts.length; i++) {
             int index = -1;
             String key = parts[i];
 
-            if (key.endsWith("]"))
-            {
+            if (key.endsWith("]")) {
                 int b = key.indexOf("[");
-                try
-                {
+                try {
                     index = Integer.parseInt(key.substring(b+1, key.length()-1));
                     key = key.substring(0, b);
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     throw new IllegalStateException("Path syntax error - invalid index");
                 }
             }
@@ -106,8 +90,7 @@ public class JSONParser {
             else
                 return null;
 
-            if (index >= 0)
-            {
+            if (index >= 0) {
                 if ((json != null) && (json instanceof List))
                     json = ((List) json).get(index);
                 else
@@ -118,10 +101,8 @@ public class JSONParser {
         return json;
     }
 
-    private static char skipSpaces(String json, int[] pos)
-    {
-        while (true)
-        {
+    private static char skipSpaces(String json, int[] pos) {
+        while (true) {
             if (pos[0] >= json.length())
                 return 0;
             char ch = json.charAt(pos[0]);
@@ -132,16 +113,13 @@ public class JSONParser {
         }
     }
 
-    private static Boolean parseBoolean(String json, int[] pos)
-    {
-        if (json.regionMatches(pos[0], "true", 0, 4))
-        {
+    private static Boolean parseBoolean(String json, int[] pos) {
+        if (json.regionMatches(pos[0], "true", 0, 4)) {
             pos[0] += 4;
             return Boolean.TRUE;
         }
 
-        if (json.regionMatches(pos[0], "false", 0, 5))
-        {
+        if (json.regionMatches(pos[0], "false", 0, 5)) {
             pos[0] += 5;
             return Boolean.FALSE;
         }
@@ -149,19 +127,16 @@ public class JSONParser {
         return null;
     }
 
-    private static Number parseNumber(String json, int[] pos)
-    {
+    private static Number parseNumber(String json, int[] pos) {
         int endPos = json.length();
         int startPos = pos[0];
 
         boolean foundExp = false;
         boolean foundDot = false;
         boolean allowPM = true;
-        for (int i=startPos; i<endPos; i++)
-        {
+        for (int i=startPos; i<endPos; i++) {
             char ch = json.charAt(i);
-            if ((ch == 'e') || (ch == 'E'))
-            {
+            if ((ch == 'e') || (ch == 'E')) {
                 if (foundExp)
                     return null;
                 allowPM = true;
@@ -169,10 +144,8 @@ public class JSONParser {
                 continue;
             }
 
-            if ((ch == '+') || (ch == '-'))
-            {
-                if (allowPM)
-                {
+            if ((ch == '+') || (ch == '-')) {
+                if (allowPM) {
                     allowPM = false;
                     ch = skipSpaces(json, pos);
                     if (ch == 0)
@@ -185,16 +158,14 @@ public class JSONParser {
             }
 
             allowPM = false;
-            if (ch == '.')
-            {
+            if (ch == '.') {
                 if (foundDot)
                     return null;
                 foundDot = true;
                 continue;
             }
 
-            if (!Character.isDigit(json.charAt(i)))
-            {
+            if (!Character.isDigit(json.charAt(i))) {
                 pos[0] = endPos = i;
                 break;
             }
@@ -204,20 +175,17 @@ public class JSONParser {
             return null;
 
         String numericString = json.substring(startPos, endPos);
-        try
-        {
+        try {
             return Integer.parseInt(numericString);
         }
         catch (Exception e) {}
 
-        try
-        {
+        try {
             return Long.parseLong(numericString);
         }
         catch (Exception e) {}
 
-        try
-        {
+        try {
             return Double.parseDouble(numericString);
         }
         catch (Exception e) {}
@@ -225,26 +193,21 @@ public class JSONParser {
         throw new IllegalStateException("Failed to parse JSON number at "+startPos+" '"+numericString+"'");
     }
 
-    private static List parseArray(String json, int[] pos)
-    {
+    private static List parseArray(String json, int[] pos) {
         int start = pos[0];
         if (json.charAt(start) != '[')
             return null;
         pos[0]++;
 
         ArrayList result = new ArrayList();
-        while (true)
-        {
+        while (true) {
             char ch = skipSpaces(json, pos);
             if (ch == 0)
                 break;
-            else if (ch == ']')
-            {
+            else if (ch == ']') {
                 pos[0]++;
                 return result;
-            }
-            else if (ch == ',')
-            {
+            } else if (ch == ',') {
                 pos[0]++;
                 if (skipSpaces(json, pos) == 0)
                     break;
@@ -256,26 +219,21 @@ public class JSONParser {
         throw new IllegalStateException("json Array format at "+start+" ["+(pos[0]-start)+"]  '"+json.substring(start)+"'");
     }
 
-    private static Map parseObject(String json, int[] pos)
-    {
+    private static Map parseObject(String json, int[] pos) {
         int start = pos[0];
         if (json.charAt(start) != '{')
             return null;
         pos[0]++;
 
         Map result = new LinkedHashMap();
-        while (true)
-        {
+        while (true) {
             char ch = skipSpaces(json, pos);
             if (ch == 0)
                 break;
-            else if (ch == '}')
-            {
+            else if (ch == '}') {
                 pos[0]++;
                 return result;
-            }
-            else if (ch == ',')
-            {
+            } else if (ch == ',') {
                 pos[0]++;
                 if (skipSpaces(json, pos) == 0)
                     break;
@@ -296,27 +254,22 @@ public class JSONParser {
         throw new IllegalStateException("json Object format at "+pos[0]+"  ["+start+", "+json.length()+"]  '"+json.substring(pos[0])+"'");
     }
 
-    private static String parseString(String json, int[] pos)
-    {
+    private static String parseString(String json, int[] pos) {
         int startPos = pos[0];
         if (json.charAt(startPos) != '"')
             return null;
         pos[0]++;
 
         boolean isEscape = false;
-        for (int i=startPos+1; i<json.length(); i++)
-        {
+        for (int i=startPos+1; i<json.length(); i++) {
             char ch = json.charAt(i);
-            if (ch == '\\')
-            {
+            if (ch == '\\') {
                 isEscape = !isEscape;
                 continue;
             }
 
-            if (ch == '"')
-            {
-                if (!isEscape)
-                {
+            if (ch == '"') {
+                if (!isEscape) {
                     pos[0] = i+1;
                     return json.substring(startPos+1, i);
                 }
@@ -328,8 +281,7 @@ public class JSONParser {
         throw new IllegalStateException("json string at at "+startPos+"  '"+json+"'");
     }
 
-    private static Object parse(String json, int[] pos)
-    {
+    private static Object parse(String json, int[] pos) {
         char ch = skipSpaces(json, pos);
         if (ch == 0)
             return null;
@@ -357,8 +309,7 @@ public class JSONParser {
         if (result != null)
             return result;
 
-        if (json.regionMatches(pos[0], "null", 0, 4))
-        {
+        if (json.regionMatches(pos[0], "null", 0, 4)) {
             pos[0] += 4;
             return null;
         }
@@ -366,11 +317,9 @@ public class JSONParser {
         throw new IllegalStateException("json object at at "+startPos+"  '"+json+"'");
     }
 
-    private static void escapeString(String s, StringBuffer buf)
-    {
+    private static void escapeString(String s, StringBuffer buf) {
         buf.append('"');
-        for (int i=0; i<s.length(); i++)
-        {
+        for (int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
             if ((ch == '"') || (ch == '\\'))
                 buf.append('\\');
@@ -379,21 +328,18 @@ public class JSONParser {
         buf.append('"');
     }
 
-    private static void toString(Object obj, StringBuffer buf)
-    {
+    private static void toString(Object obj, StringBuffer buf) {
         if (obj == null)
             buf.append("null");
         else if ((obj instanceof Boolean) || (obj instanceof Number))
-            buf.append(obj.toString());
-        else if (obj instanceof Map)
-        {
+            buf.append(obj);
+        else if (obj instanceof Map) {
             Map m = (Map) obj;
             boolean first = true;
             Iterator itt = m.keySet().iterator();
 
             buf.append('{');
-            while (itt.hasNext())
-            {
+            while (itt.hasNext()) {
                 if (!first)
                     buf.append(',');
 
@@ -405,15 +351,12 @@ public class JSONParser {
                 first = false;
             }
             buf.append('}');
-        }
-        else if (obj instanceof Object[])
-        {
+        } else if (obj instanceof Object[]) {
             Object[] l = (Object[]) obj;
             boolean first = true;
 
             buf.append('[');
-            for (int i=0; i<l.length; i++)
-            {
+            for (int i = 0; i < l.length; i++) {
                 if (!first)
                     buf.append(',');
 
@@ -421,16 +364,13 @@ public class JSONParser {
                 first = false;
             }
             buf.append(']');
-        }
-        else if (obj instanceof List)
-        {
+        } else if (obj instanceof List) {
             List l = (List) obj;
             boolean first = true;
             Iterator itt = l.iterator();
 
             buf.append('[');
-            while (itt.hasNext())
-            {
+            while (itt.hasNext()) {
                 if (!first)
                     buf.append(',');
 
@@ -439,11 +379,11 @@ public class JSONParser {
                 first = false;
             }
             buf.append(']');
-        }
-        else if (obj instanceof String)
+        } else if (obj instanceof String) {
             escapeString(obj.toString(), buf);
-        else
-        {
+        } else if(obj instanceof JSONSerializable) {
+            buf.append(((JSONSerializable)obj).toJSON());
+        } else {
             try
             {
                 Class cls = obj.getClass();
